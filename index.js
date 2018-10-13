fbd4e39360ed8bd8ba2ccfa32b6ffc5bf9a2b4b3
Denis Coric <dcoric@groundlink.com>
2018-10-13 13:32:37 +0200

    Response object improved

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const expressip = require('express-ip');
 const PORT = process.env.PORT || 5000;
 const path = require('path');
+const {get} = require('lodash');
+
 
 app.use(expressip().getIpInfoMiddleware);
 
@@ -11,9 +13,16 @@ app.set('PORT', PORT);
 
 app.get('/', (req, res) => {
     const ipInfo = req.ipInfo;
+    let responseData = req.ipInfo;
+    responseData.country_code = ipInfo.country;
+    responseData.country_name = ipInfo.country;
+    responseData.city = ipInfo.city;
+    responseData.longitude = ipInfo.ll[0];
+    responseData.latitude = ipInfo.ll[1];
+    responseData.IPv4 = get(req, 'headers.x-forwarded-for');
     var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
     console.log(message);
-    res.send(ipInfo);
+    res.send(responseData);
 });
 
 app.listen(app.get('PORT'), () => {
