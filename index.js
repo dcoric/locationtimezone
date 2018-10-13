6564f340cc72c46052ab7fb698655e499230bf5b
Denis Coric <dcoric@groundlink.com>
2018-10-13 13:36:24 +0200

    Response object improved - lat long

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ app.set('PORT', PORT);
 
 app.get('/', (req, res) => {
     const ipInfo = req.ipInfo;
-    let responseData = req.ipInfo;
-    responseData.country_code = ipInfo.country;
-    responseData.country_name = ipInfo.country;
-    responseData.city = ipInfo.city;
-    responseData.longitude = ipInfo.ll[0];
-    responseData.latitude = ipInfo.ll[1];
-    responseData.IPv4 = get(req, 'headers.x-forwarded-for');
+    let responseData = {
+        country_code: ipInfo.country,
+        country_name: ipInfo.country,
+        city: ipInfo.city,
+        latitude: ipInfo.ll[0],
+        longitude: ipInfo.ll[1],
+        IPv4: get(req, 'headers.x-forwarded-for')
+    }
     var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
     console.log(message);
     res.send(responseData);
