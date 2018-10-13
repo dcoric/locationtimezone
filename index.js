7e18912ca3bc31bc9186319f963edcd38bcc20a7
Denis Coric <dcoric@groundlink.com>
2018-10-13 14:14:11 +0200

    Error handeling

--- index.js
+++ index.js
@@ -12,21 +12,25 @@ app.use(expressip().getIpInfoMiddleware);
 app.set('PORT', PORT);
 
 app.get('/', (req, res) => {
-    const ipInfo = req.ipInfo;
-    let responseData = {
-        country_code: ipInfo.country,
-        country_name: ipInfo.country,
-        city: ipInfo.city,
-        latitude: ipInfo.ll[0],
-        longitude: ipInfo.ll[1],
-        IPv4: get(req, 'headers.x-forwarded-for'),
-        eu: ipInfo.country === 'RS' ? "1" : ipInfo.eu,
-        region: ipInfo.region,
-        timezone: ipInfo.timezone
+    try {
+        const ipInfo = req.ipInfo;
+        let responseData = {
+            country_code: ipInfo.country,
+            country_name: ipInfo.country,
+            city: ipInfo.city,
+            latitude: ipInfo.ll[0],
+            longitude: ipInfo.ll[1],
+            IPv4: get(req, 'headers.x-forwarded-for'),
+            eu: ipInfo.eu,
+            region: ipInfo.region,
+            timezone: ipInfo.timezone
+        }
+        var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
+        console.log(message);
+        res.send(responseData);
+    } catch (exception) {
+        res.status(500).send('Error processing IP address');
     }
-    var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
-    console.log(message);
-    res.send(responseData);
 });
 
 app.listen(app.get('PORT'), () => {
