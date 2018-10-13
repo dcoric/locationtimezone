3c8b206fcc69e7f93226c0aa28d6f911e42ba90a
Denis Coric <dcoric@groundlink.com>
2018-10-13 13:10:22 +0200

    Experimentation with request

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({status: 'Working!'});
+    console.info('Request:', req.headers);
+    const headers = req.headers;
+    res.send({status: 'Working!', receivedHeaders: headers});
 });
 
 const PORT = process.env.PORT || 5000;
