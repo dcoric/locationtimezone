e1a12344dc5ac529018e7dedf5deb8600a4981f9
Denis Coric <dcoric@groundlink.com>
2018-10-15 23:51:35 +0200

    Temp cors enabled

--- src/app.js
+++ src/app.js
@@ -7,23 +7,23 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(expressip().getIpInfoMiddleware);
-let whitelist = [
-  'http://localhost:3000',
-  'https://brio.groundlink.com',
-  'https://gq-brio.groundlink.com',
-  'https://brio.davelbostoncoach.com',
-  'https://gq-brio.davelbostoncoach.com',
-];
-let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-app.use(cors(corsOptions));
+// let whitelist = [
+//   'http://localhost:3000',
+//   'https://brio.groundlink.com',
+//   'https://gq-brio.groundlink.com',
+//   'https://brio.davelbostoncoach.com',
+//   'https://gq-brio.davelbostoncoach.com',
+// ];
+// let corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// };
+app.use(cors());
 app.set('PORT', PORT);
 
 app.listen(PORT, () => {
